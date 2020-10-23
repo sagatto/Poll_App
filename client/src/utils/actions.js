78b61bb83de22e63c1365bac5e8d0548d83223b9
Mukul Sharma <msharma@3222-mbp15.attlocal.net>
2020-10-22 20:16:40 -0700

    Adding files in client/utils

---
+++ client/src/utils/actions.js
@@ -0,0 +1,5 @@
+export const ADD_POLL = "ADD_POLL";
+export const DELETE_POLL = 'DELETE_POLL';
+export const CAST_VOTE = "CAST_VOTE";
+export const VOTE_COUNT = 'VOTE_COUNT';
+export const CLEAR_POLL = 'CLEAR_POLL';
