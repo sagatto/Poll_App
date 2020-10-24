b8c1f41900fe3889bf7bdffec401139772da8766
Sparsh Patwa <patwa.sparsh@gmail.com>
2020-10-24 01:54:23 -0700

    Add state management(actions, reducers) and test actions

--- client/src/utils/GlobalState.js
+++ client/src/utils/GlobalState.js
@@ -1,18 +1,16 @@
 import React, { createContext, useContext } from "react";
-import { useProductReducer } from './reducers';
+import { usePollReducer } from './reducers';
 
 const StoreContext = createContext();
 const { Provider } = StoreContext;
 
 const StoreProvider = ({ value = [], ...props }) => {
-    const [state, dispatch] = useProductReducer({
-        products: [],
-        cart: [],
-        cartOpen: false,
-        categories: [],
-        currentCategory: ''
+    const [state, dispatch] = usePollReducer({
+        // Instantiate inital global state
+        allPolls: [],
+        selectedPoll: {},
+        newPoll: {}
       });
-    // use this to confirm it works!
     console.log(state);
     return <Provider value={[state, dispatch]} {...props} />;
   };
--- client/src/utils/reducers.js
+++ client/src/utils/reducers.js
@@ -1,83 +1,32 @@
 import { useReducer } from "react";
 import {
-  UPDATE_PRODUCTS,
-  UPDATE_CATEGORIES,
-  UPDATE_CURRENT_CATEGORY,
-  ADD_TO_CART,
-  ADD_MULTIPLE_TO_CART,
-  REMOVE_FROM_CART,
-  UPDATE_CART_QUANTITY,
-  CLEAR_CART,
-  TOGGLE_CART,
+  UPDATE_POLLS,
+  SELECTED_POLL,
+  CREATE_POLL,
 } from "./actions";
 
 export const reducer = (state, action) => {
   switch (action.type) {
-    // if action type value is the value of `UPDATE_PRODUCTS`, return a new state object with an updated products array
-    case UPDATE_PRODUCTS:
+    case UPDATE_POLLS:
       return {
         ...state,
-        products: [...action.products],
+        allPolls: [ ...action.allPolls ],
       };
-    // if action type value is the value of `UPDATE_CATEGORIES`, return a new state object with an updated categories array
-    case UPDATE_CATEGORIES:
+    case SELECTED_POLL:
       return {
         ...state,
-        categories: [...action.categories],
+        selectedPoll: { ...action.selectedPoll },
       };
-    case UPDATE_CURRENT_CATEGORY:
-      return {
-        ...state,
-        currentCategory: action.currentCategory,
-      };
-    case ADD_TO_CART:
-      return {
-        ...state,
-        cartOpen: true,
-        cart: [...state.cart, action.product],
-      };
-    case ADD_MULTIPLE_TO_CART:
-      return {
-        ...state,
-        cart: [...state.cart, ...action.products],
-      };
-    case REMOVE_FROM_CART:
-      let newState = state.cart.filter((product) => {
-        return product._id !== action._id;
-      });
-
-      return {
-        ...state,
-        cartOpen: newState.length > 0,
-        cart: newState,
-      };
-    case UPDATE_CART_QUANTITY:
-      return {
-        ...state,
-        cartOpen: true,
-        cart: state.cart.map((product) => {
-          if (action._id === product._id) {
-            product.purchaseQuantity = action.purchaseQuantity;
-          }
-          return product;
-        }),
-      };
-    case CLEAR_CART:
-      return {
-        ...state,
-        cartOpen: false,
-        cart: [],
-      };
-    case TOGGLE_CART:
-      return {
-        ...state,
-        cartOpen: !state.cartOpen,
-      };
-
+      case CREATE_POLL:
+        return {
+          ...state,
+          newPoll: { ...action.newPoll },
+        };
     default:
       return state;
   }
 };
-export function useProductReducer(initialState) {
+
+export function usePollReducer(initialState) {
   return useReducer(reducer, initialState);
 }
