609c9978d4e3fa5fb550fb59d8154b3f59cafe6e
Sparsh Patwa <patwa.sparsh@gmail.com>
2020-10-29 12:10:21 -0700

    Graphql support to frontend pages

--- client/src/pages/CreateQuestion.js
+++ client/src/pages/CreateQuestion.js
@@ -1,38 +1,68 @@
-import React from 'react';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { useMutation } from "@apollo/react-hooks";
+import Auth from "../utils/auth";
+import { ADD_POLL } from "../utils/mutations";
 import {
-    Flex,
-    Box,
-    Heading,
-    FormControl,
-    Textarea,
-    FormLabel,
-    Input,
-    Button
-  } from '@chakra-ui/core';
-
-  
+  Flex,
+  Box,
+  Heading,
+  FormControl,
+  Textarea,
+  FormLabel,
+  Input,
+  Button,
+} from "@chakra-ui/core";
 
 function CreateQuestion() {
-    return (
-      <Flex width="full" align="center" justifyContent="center">
-        <Box p={8} maxWidth="80%" borderWidth={1} borderRadius={8} boxShadow="lg">
-          <Box textAlign="center">
-            <Heading>Post a Question for the CEO</Heading>
-          </Box>
-          <Box my={4} textAlign="left">
-            <form>
-              <FormControl>
-                <FormLabel></FormLabel>
-                <Textarea type="text" wrap="wrap" placeholder="Enter your question here!" height="100px" />
-              </FormControl>
-              <Button type="submit" variantColor="purple" variant="outline" width="full" mt={4}>
-                Add Anonymously
-              </Button>
-            </form>
-          </Box>
-        </Box>
-      </Flex>
-    );
-  }
+  const [formState, setFormState] = useState({ question: "" });
+  const [addPoll] = useMutation(ADD_POLL);
 
-  export default CreateQuestion;
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const mutationResponse = await addPoll({
+        variables: { question: formState.question },
+      });
+      const token = mutationResponse.data.login.token;
+      Auth.login(token);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  return (
+    <Flex width="full" align="center" justifyContent="center">
+      <Box p={8} maxWidth="80%" borderWidth={1} borderRadius={8} boxShadow="lg">
+        <Box textAlign="center">
+          <Heading>Post a Question for the CEO</Heading>
+        </Box>
+        <Box my={4} textAlign="left">
+          <form onSubmit={handleFormSubmit}>
+            <FormControl>
+              <FormLabel></FormLabel>
+              <Textarea
+                name="question"
+                type="text"
+                wrap="wrap"
+                placeholder="Enter your question here!"
+                height="100px"
+              />
+            </FormControl>
+            <Button
+              type="submit"
+              variantColor="purple"
+              variant="outline"
+              width="full"
+              mt={4}
+            >
+              Add Anonymously
+            </Button>
+          </form>
+        </Box>
+      </Box>
+    </Flex>
+  );
+}
+
+export default CreateQuestion;
