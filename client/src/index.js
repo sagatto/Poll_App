38ed349b898b35df6e00b80514139e525ed0fe7b
Sparsh Patwa <patwa.sparsh@gmail.com>
2020-10-29 12:11:37 -0700

    Various bug fixes to frontend

--- client/src/index.js
+++ client/src/index.js
@@ -8,6 +8,7 @@ import {
     Input,
     Button
   } from '@chakra-ui/core';
+
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
@@ -23,4 +24,4 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.register();
