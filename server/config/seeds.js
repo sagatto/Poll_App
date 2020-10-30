0af4a6f4d73c4091e05b4a7065362b882422a4c2
Sparsh Patwa <patwa.sparsh@gmail.com>
2020-10-29 23:05:03 -0700

    Fix seeds to match schema

--- server/config/seeds.js
+++ server/config/seeds.js
@@ -1,44 +1,37 @@
 const faker = require('faker');
-
 const db = require('../config/connection');
 const { Poll, User } = require('../models');
-
+// Debug enable 
+const debug = 0
+// Number of fakers for seeds
+const numFakers = 5;
 db.once('open', async () => {
   await Poll.deleteMany({});
   await User.deleteMany({});
 
-  // create user data
+  // create user data for Users collection
   const userData = [];
-
-  for (let i = 0; i < 50; i += 1) {
+  for (let i = 0; i < numFakers; i += 1) {
     const username = faker.internet.userName();
     const email = faker.internet.email(username);
     const password = faker.internet.password();
-
-    userData.push({ username, email, password });
+    userData.push({ email, password });
   }
-
   const createdUsers = await User.collection.insertMany(userData);
-
-
-  // create Polls
+  // create polls for Polls collections
   let createdPolls = [];
-  for (let i = 0; i < 100; i += 1) {
+  for (let i = 0; i < numFakers; i += 1) {
     const question = faker.lorem.words(Math.round(Math.random() * 20) + 1);
-
     const randomUserIndex = Math.floor(Math.random() * createdUsers.ops.length);
-    const { username, _id: userId } = createdUsers.ops[randomUserIndex];
-
-    const poll = await Poll.create({ question, username });
-
+    const {  _id: userId } = createdUsers.ops[randomUserIndex];
+    const poll = await Poll.create({ question });
     const updatedUser = await User.updateOne(
       { _id: userId },
       { $push: { polls: poll._id } }
     );
-
     createdPolls.push(poll);
   }
   
-  console.log('all done!');
+  if(debug) console.log('Completed seeding your DB');
   process.exit(0);
 });
