2c7192553f2154c39fe25bb1a1c9dbd125c0aaea
Sparsh Patwa <patwa.sparsh@gmail.com>
2020-10-30 20:38:38 -0700

    Fix Dory page question list frontend functionality

--- client/src/pages/ViewQuestions.js
+++ client/src/pages/ViewQuestions.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   Flex,
   Box,
@@ -10,8 +10,65 @@ import {
   Button,
 } from "@chakra-ui/core";
 import Auth from "../utils/auth";
+import { useMutation, useQuery } from "@apollo/react-hooks";
+import { ALL_POLLS } from "../utils/queries";
+import { LIKE, DISLIKE } from "../utils/mutations";
+// Debug enable
+const debug = 1;
 
 function ViewQuestions() {
+  const [formState, setFormState] = useState({ count: "" });
+  const { loading, data } = useQuery(ALL_POLLS);
+  const doryPolls = data?.allPolls || {};
+  if(debug) console.log("All Polls:",doryPolls);
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+  //if(debug) console.log("All Polls2:",doryPolls[0].question);
+
+  // Poll list logic
+  // const refreshPolls = async (event) => {
+  //   event.preventDefault();
+  //   try {
+  //     //const doryPolls = await allPolls();
+  //     const { loading, data: doryPolls } = useQuery(ALL_POLLS);
+  //     if(debug) console.log("All Polls:",doryPolls);
+  //   } catch (e) {
+  //     console.log(e);
+  //   }
+  // };
+
+  // // Like logic
+  // const [addLike] = useMutation(LIKE);
+  // const [addDislike] = useMutation(DISLIKE);
+
+  // const handleLike = async (event) => {
+  //   event.preventDefault();
+  //   try {
+  //     const mutationResponse = await addLike({
+  //       variables: { question: formState.question },
+  //     });
+  //   } catch (e) {
+  //     console.log(e);
+  //   }
+  // };
+  // const handleDislike = async (event) => {
+  //   event.preventDefault();
+  //   try {
+  //     const mutationResponse = await addDislike({
+  //       variables: { question: formState.question },
+  //     });
+  //   } catch (e) {
+  //     console.log(e);
+  //   }
+  // };
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormState({
+      ...formState,
+      [name]: value,
+    });
+  };
   return (
     <Flex width="full" align="center" justifyContent="center">
       {Auth.loggedIn() ? (
@@ -20,6 +77,8 @@ function ViewQuestions() {
             <Box textAlign="center">
               <Heading>Questions</Heading>
             </Box>
+            {doryPolls && doryPolls.map(doryPolls => (
+            <div key={doryPolls._id} >
             <Box
               my={4}
               textAlign="center"
@@ -28,48 +87,46 @@ function ViewQuestions() {
               borderRadius={8}
               boxShadow="lg"
             >
-            <form>
-              <Text>
-                How was the latest funding round ?
-              </Text>
-              <Button
-              type="submit"
-              variantColor="purple"
-              variant="outline"
-              width="25"
-              margin="3px"
-              mt={4}
-              >
-                👍
-              </Button>
-              <Button
-              type="submit"
-              variantColor="purple"
-              variant="outline"
-              width="25"
-              margin="3px"
-              mt={4}
-              >
-                👎
-              </Button>
-              <Button
-              type="submit"
-              variantColor="purple"
-              variant="outline"
-              width="25"
-              margin="3px"
-              mt={4}
-              >
-                Upvotes: 50
-              </Button>
-            </form>
-          </Box>
+              <form>
+                <Text>{doryPolls.question}</Text>
+                <Button
+                  type="submit"
+                  variantColor="purple"
+                  variant="outline"
+                  width="25"
+                  margin="3px"
+                  mt={4}
+                >
+                  👍
+                </Button>
+                <Button
+                  type="submit"
+                  variantColor="purple"
+                  variant="outline"
+                  width="25"
+                  margin="3px"
+                  mt={4}
+                >
+                  👎
+                </Button>
+                <Button
+                  type="submit"
+                  variantColor="purple"
+                  variant="outline"
+                  width="25"
+                  margin="3px"
+                  mt={4}
+                >
+                  Upvotes: {doryPolls.count} 
+                </Button>
+              </form>
+            </Box>
+            </div>
+          ))}
         </Box>
-      </div>
-      ) : (
-        <div>
-          Login to View Dory Questions
         </div>
+      ) : (
+        <div>Login to View Dory Questions</div>
       )}
     </Flex>
   );
